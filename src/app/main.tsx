ebd0cb9610ae53dc5655f52e0903915a41a3e0f1
Carlos Quiroz Muñoz <carlos.quiroz25@icloud.com>
2026-06-14 16:38:19 -0500

    feat: se agrega componente de Vercel para analíitica de la web

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot, hydrateRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
@@ -6,6 +7,7 @@ import { App } from "./App";
 const app = (
   <StrictMode>
     <App />
+    <Analytics /> 
   </StrictMode>
 );
 
